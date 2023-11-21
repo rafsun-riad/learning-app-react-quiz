df47620b51db8fd3c55ebd9139b5eaab348b707f
rafsun-riad <mruhaquer@gmail.com>
2023-11-21 20:49:15 +0600

    started quiz app and loaded qustions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+	questions: [],
+	// 'loading', 'error', 'ready', 'active', 'finished'
+	status: 'loading',
+};
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceive':
+			return {
+				...state,
+				questions: action.payload,
+				status: 'ready',
+			};
+		case 'dataFailed':
+			return {
+				...state,
+				status: 'error',
+			};
+		default:
+			throw new Error('Action unknown');
+	}
+}
 
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	useEffect(() => {
+		fetch('http://localhost:9000/questions')
+			.then((res) => res.json())
+			.then((data) => dispatch({ type: 'dataReceive', payload: data }))
+			.catch((err) => dispatch({ type: 'dataFailed' }));
+	}, []);
 	return (
-		<div>
-			<DateCounter />
+		<div className="app">
+			<Header />
+			<Main>
+				<p>1/15</p>
+				<p>Question?</p>
+			</Main>
 		</div>
 	);
 }
