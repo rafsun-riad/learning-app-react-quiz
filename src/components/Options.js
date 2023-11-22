df418555be466bb29c2f5d37b76e0803d666cafc
rafsun-riad <mruhaquer@gmail.com>
2023-11-22 21:09:07 +0600

    created component folder and finished question render

---
+++ src/components/Options.js
@@ -0,0 +1,29 @@
+function Options({ questions, dispatch, answer }) {
+	const hasAnswered = answer !== null;
+	return (
+		<div>
+			{questions.options.map((option, index) => (
+				<button
+					className={`btn btn-option ${
+						index === answer ? 'answer' : ''
+					} ${
+						hasAnswered
+							? index === questions.correctOption
+								? 'correct'
+								: 'wrong'
+							: ''
+					}`}
+					key={option}
+					disabled={hasAnswered}
+					onClick={() =>
+						dispatch({ type: 'newAnswer', payload: index })
+					}
+				>
+					{option}
+				</button>
+			))}
+		</div>
+	);
+}
+
+export default Options;
---
+++ src/components/Question.js
@@ -0,0 +1,16 @@
+import Options from './Options';
+function Question({ questions, dispatch, answer }) {
+	console.log(questions);
+	return (
+		<div>
+			<h4>{questions.question}</h4>
+			<Options
+				questions={questions}
+				dispatch={dispatch}
+				answer={answer}
+			/>
+		</div>
+	);
+}
+
+export default Question;
